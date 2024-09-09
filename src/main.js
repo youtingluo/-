fdb09db57c2c22d7cfe0f28e1695b68055bb205a
阿沛 <apei@apeideMBP.home>
2024-09-09 12:30:31 +0800

    套用bs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/scss/all.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,20 @@
 <template>
   <main>
     <h1>home</h1>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary">Left</button>
+      <button type="button" class="btn btn-primary">Middle</button>
+      <button type="button" class="btn btn-primary">Right</button>
+    </div>
+
+    <button
+      type="button"
+      class="btn btn-secondary"
+      data-bs-toggle="tooltip"
+      data-bs-placement="top"
+      data-bs-title="Tooltip on top"
+    >
+      Tooltip on top
+    </button>
   </main>
 </template>
