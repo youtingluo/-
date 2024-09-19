2fe43969fba597f415c50dcc062f9e50bcf96ada
阿沛 <apei@apeideMBP.home>
2024-09-19 09:18:17 +0800

    調整選取色系

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,10 @@ import { RouterLink, RouterView } from 'vue-router'
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <RouterLink class="nav-link active" aria-current="page" to="/">內頁</RouterLink>
+                <RouterLink class="nav-link active" aria-current="page" to="/">首頁</RouterLink>
               </li>
               <li class="nav-item">
-                <RouterLink class="nav-link" aria-current="page" to="/company">廠商</RouterLink>
+                <RouterLink class="nav-link" aria-current="page" to="/company">內頁</RouterLink>
               </li>
             </ul>
           </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,96 +1,216 @@
+<script setup>
+import axios from 'axios'
+import { computed, onMounted, ref } from 'vue'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/css/index.css'
+const apiKey = import.meta.env.VITE_APP_APIKEY
+const sheetId = import.meta.env.VITE_APP_SHEETID
+// 取得資料
+async function getSheetData(industry = '火鍋') {
+  isLoading.value = true
+  selectedindustry.value = industry
+  const range = `${industry}!A1:O50`
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
+  try {
+    const response = await axios.get(url)
+    isLoading.value = false
+    const values = response.data.values
+    keys.value = values[0].slice(1, -1)
+    hotpot.value = convertToObjects(values)
+    console.log(hotpot.value)
+    getType(keys.value[0])
+  } catch (error) {
+    isLoading.value = false
+    console.error('Error fetching values:', error)
+  }
+}
+// 轉換成陣列包物件的函數
+function convertToObjects(array) {
+  const keys = array[0]
+  const result = []
+  for (let i = 1; i < array.length; i++) {
+    const obj = {}
+    for (let j = 0; j < keys.length; j++) {
+      obj[keys[j]] = array[i][j]
+    }
+    result.push(obj)
+  }
+  return result
+}
+
+const isLoading = ref(false)
+const hotpot = ref([])
+// 取得物件 KEY
+const keys = ref([])
+// 篩選廠商
+const selectedindustry = ref('')
+const selectedType = ref('')
+const selected = ref('')
+const filterType = ref([])
+const filterCompany = computed(() => {
+  if (!selected.value) {
+    return hotpot.value.filter((item) => item[selectedType.value])
+  }
+  return hotpot.value.filter((item) => item[selectedType.value].toString().includes(selected.value))
+})
+// 合併陣列
+const getType = (objKey) => {
+  selected.value = ''
+  const type = []
+  selectedType.value = objKey
+  hotpot.value.forEach((item) => {
+    // 要合併每個物件的陣列
+    type.push(item[selectedType.value])
+  })
+  // 移除重複陣列資料
+  filterType.value = [...new Set(type.filter(Boolean).join().split(','))]
+}
+// -------- end -------
+onMounted(() => {
+  getSheetData()
+})
+</script>
 <template>
+  <Loading v-model:active="isLoading">
+    <div class="loader"></div>
+  </Loading>
   <div class="container">
-    <div class="bg-secondary p-3 mb-4 rounded-5">
-      <h2 class="fs-5">
-        <a href="#" class="pe-3">開南食品</a>
-        <a href="#"><i class="bi bi-heart"></i></a>
-      </h2>
+    <h3 class="fs-6">行業</h3>
+    <ul class="d-flex fs-3 mb-3">
+      <li>
+        <button
+          type="button"
+          class="btn btn-outline-primary border-0"
+          :class="{ active: selectedindustry === '飲料' }"
+          @click="getSheetData('飲料')"
+        >
+          飲料店
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          class="btn btn-outline-primary border-0"
+          :class="{ active: selectedindustry === '火鍋' }"
+          @click="getSheetData('火鍋')"
+        >
+          火鍋店
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          class="btn btn-outline-primary border-0"
+          :class="{ active: selectedindustry === '挫冰店' }"
+          @click="selectedindustry = '挫冰店'"
+        >
+          挫冰店
+        </button>
+      </li>
+      <!-- <li>
+        <button
+          type="button"
+          class="btn btn-outline-primary border-0"
+          :class="{ active: selectedindustry === '燒烤店' }"
+          @click="selectedindustry = '燒烤店'"
+        >
+          燒烤店
+        </button>
+      </li>
+      <li>
+        <button
+          type="button"
+          class="btn btn-outline-primary border-0"
+          :class="{ active: selectedindustry === '挫冰店' }"
+          @click="selectedindustry = '挫冰店'"
+        >
+          挫冰店
+        </button>
+      </li> -->
+    </ul>
 
-      <ul class="ps-0 d-flex">
-        <li>
-          <span class="badge rounded-pill text-bg-primary text-secondary px-3 me-3">茶葉</span>
-        </li>
-        <li>
-          <span class="badge rounded-pill text-bg-primary text-secondary px-3 me-3">茶葉</span>
-        </li>
-        <li>
-          <span class="badge rounded-pill text-bg-primary text-secondary px-3 me-3">茶葉</span>
-        </li>
-      </ul>
-      <div>
-        <h3 class="mt-5 fs-5">公司介紹</h3>
-        <p>
-          開南食品成立於1986年， 從販售珍珠奶茶原物料起家， 深耕產業三十多年來，
-          堅守食品安全的第一防線，秉持著安心飲食的精神，不斷開發與推出新產品， 致力於提供顧客最好、
-          最安全的食品烘焙、 珍珠奶茶原物料， 也協助了無數茶飲、餐飲店家成功創業，
-          未來我們會繼續傾聽您的聲音， 與您分享最新的產品及新知， 持續追求100%
-          的顧客滿意度，朝向更專業服務邁進。
-        </p>
+    <div class="row mb-3">
+      <div class="col">
+        <h3 class="fs-6">類型</h3>
+        <ul class="d-flex fs-3 flex-wrap">
+          <li v-for="objKey in keys" :key="objKey">
+            <a
+              href="#"
+              class="btn btn-outline-primary border-0"
+              :class="{ active: selectedType === objKey }"
+              @click.prevent="getType(objKey)"
+              >{{ objKey }}</a
+            >
+          </li>
+        </ul>
       </div>
     </div>
-
-    <div class="text-bg-primary text-secondary p-3 mb-3 rounded-5">
-      <h2 class="fs-5">
-        實際評測
-        <img src="../assets/r-Photoroom.png" alt="" />
-      </h2>
-      <ul class="ps-0 d-flex">
-        <li>
-          <button
-            type="button"
-            class="review-btn btn btn-secondary text-primary badge rounded-pill me-3 active"
-          >
-            茶葉
-          </button>
-          <button
-            type="button"
-            class="review-btn btn btn-secondary text-primary badge rounded-pill me-3"
-          >
-            茶葉
-          </button>
-        </li>
-        <li>
-          <button
-            type="button"
-            class="review-btn btn btn-secondary text-primary badge rounded-pill me-3"
-          >
-            茶葉
-          </button>
-        </li>
-        <li>
-          <button
-            type="button"
-            class="review-btn btn btn-secondary text-primary badge rounded-pill me-3"
-          >
-            茶葉
-          </button>
-        </li>
-      </ul>
-
-      <div class="text-info mb-3">
-        <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i
-        ><i class="bi bi-star-half"></i>
-      </div>
-      <p>
-        綠茶泡開後有明顯的苦澀味，不能放超過四個小時，否則會變酸。茶葉品質中等，適合搭配蜂蜜調味
-      </p>
-      <h2 class="fs-5">同意評價數</h2>
-      <div>
-        <div class="d-inline-block me-1">
-          <img class="rounded-circle" src="../assets/Shape.png" alt="avatar" />
-        </div>
-
-        <div class="d-inline-block me-1">
-          <img class="rounded-circle" src="../assets/Shape.png" alt="avatar" />
-        </div>
-
-        <span class="badge text-bg-light px-1">+1</span>
+    <div class="row mb-3">
+      <div class="col">
+        <h3 class="fs-6">種類</h3>
+        {{ selected }}
+        <ul class="d-flex flex-wrap fs-3">
+          <li v-for="item in filterType" :key="item">
+            <a
+              href="#"
+              class="btn btn-outline-primary border-0"
+              :class="{ active: selected === item }"
+              @click.prevent="selected = item"
+              >{{ item }}</a
+            >
+          </li>
+        </ul>
       </div>
     </div>
-
-    <div>
-      <a href="#" class="btn btn-info rounded-5 me-3">同意評價</a>
-      <RouterLink class="btn btn-info rounded-5" to="/">回到首頁</RouterLink>
+    <h3 class="fs-6">廠商</h3>
+    <div class="row">
+      <div class="col-6 col-lg-3 mb-3" v-for="company in filterCompany" :key="company['編號']">
+        <div class="bg-secondary py-3 rounded-5 h-100">
+          <div class="d-flex justify-content-between px-4 mb-3">
+            <div>
+              <a :href="company['網址']" target="_blank" class="pe-3">{{ company['廠商'] }}</a>
+            </div>
+            <div>
+              <a href="#"><i class="bi bi-heart"></i></a>
+            </div>
+          </div>
+          <div class="row justify-content-between">
+            <template v-for="value in Object.keys(company)" :key="value">
+              <div
+                class="col-6"
+                v-if="company[value] && value !== '編號' && value !== '廠商' && value !== '網址'"
+              >
+                <div class="text-center px-2">
+                  <button
+                    type="button"
+                    class="btn btn-primary text-secondary rounded-pill mb-3"
+                    :class="[
+                      { 'bg-warning': value === selectedType },
+                      { 'text-dark': value === selectedType }
+                    ]"
+                  >
+                    {{ value }}
+                  </button>
+                </div>
+              </div></template
+            >
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<style>
+.loader {
+  width: 120px;
+  height: 20px;
+  border-radius: 20px;
+  background: linear-gradient(orange 0 0) 0/0% no-repeat lightblue;
+  animation: l2 2s infinite steps(10);
+}
+@keyframes l2 {
+  100% {
+    background-size: 110%;
+  }
+}
+</style>
