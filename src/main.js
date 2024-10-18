79cf900de2ff7daf9d2db953a2392be6738387a7
youting <boy80314@gmail.com>
2024-10-18 15:53:54 +0800

    投稿與驗證完成

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-
 import './assets/scss/all.scss'
 import 'vue-loading-overlay/dist/css/index.css'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { Form, Field, ErrorMessage, defineRule, configure } from 'vee-validate'
+import { required, email, min, numeric } from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+defineRule('email', email)
+defineRule('required', required)
+defineRule('min', min)
+defineRule('numeric', numeric)
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true // 當輸入任何內容直接進行驗證
+})
+setLocale('zh_TW')
+
 const firebaseConfig = {
   apiKey: 'AIzaSyD5bMMUbcVKn0XhDjWNzZOZ0_YQwQjd2nU',
   authDomain: 'supply0-c8814.firebaseapp.com',
@@ -27,6 +40,9 @@ const authPromise = new Promise((resolve) => {
 })
 const app = createApp(App)
 app.use(router)
+app.component('VForm', Form)
+app.component('VField', Field)
+app.component('ErrorMessage', ErrorMessage)
 app.mount('#app')
 
 export { firebaseApp, auth, user, authPromise }
--- src/views/member/ContributeView.vue
+++ src/views/member/ContributeView.vue
@@ -11,125 +11,178 @@
     </div>
   </nav>
   <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-3">
       <div class="col-lg-8">
-        <form @submit.prevent="handleSubmit">
+        <VForm ref="sendForm" @submit="onSubmit" v-slot="{ errors }">
           <div class="mb-3">
-            {{ form }}
             <label for="contactName" class="form-label"
               >聯絡人名稱<span class="text-danger">*</span></label
             >
-            <input
+            <VField
+              name="聯絡人"
               type="text"
               class="form-control"
               id="contactName"
               placeholder="聯絡人名稱"
-              v-model="form.name"
+              v-model.trim="form.name"
+              rules="required"
+              :class="{ 'is-invalid': errors['聯絡人'] }"
             />
+            <ErrorMessage name="聯絡人" class="invalid-feedback" />
           </div>
           <div class="mb-3">
             <label for="company" class="form-label">代表單位(公司)</label>
-            <input
+            <VField
+              name="company"
               type="text"
               class="form-control"
               id="company"
               placeholder="單位名稱"
-              v-model="form.company"
+              v-model.trim="form.company"
             />
           </div>
           <div class="mb-3">
-            <label for="email" class="form-label"
-              >聯絡信箱 <span class="text-danger">*</span></label
-            >
-            <input
+            <label for="email" class="form-label">聯絡信箱<span class="text-danger">*</span></label>
+            <VField
+              name="聯絡信箱"
               type="email"
               class="form-control"
               id="email"
               placeholder="聯絡信箱"
-              v-model="form.email"
+              v-model.trim="form.email"
+              rules="required|email"
+              :class="{ 'is-invalid': errors['聯絡信箱'] }"
             />
+            <ErrorMessage name="聯絡信箱" class="invalid-feedback" />
           </div>
           <div class="mb-3">
             <label for="tel" class="form-label">聯絡電話</label>
-            <input
-              type="text"
+            <VField
+              name="聯絡電話"
+              type="tel"
               class="form-control"
               id="tel"
               placeholder="聯絡電話"
-              v-model="form.phone"
+              v-model.number.trim="form.phone"
+              rules="numeric|min:8"
+              :class="{ 'is-invalid': errors['聯絡電話'] }"
             />
+            <ErrorMessage name="聯絡電話" class="invalid-feedback" />
           </div>
           <hr />
-
-          <select class="form-select mb-3" aria-label="回饋方案" v-model="form.type">
+          <VField
+            @change="cleanType"
+            class="form-select mb-3"
+            name="回饋方案"
+            aria-label="回饋方案"
+            v-model="form.type"
+            rules="required"
+            :class="{ 'is-invalid': errors['回饋方案'] }"
+            as="select"
+          >
             <option disabled value="">選擇回饋方案</option>
-            <option>合作聯繫</option>
-            <option>實際評測</option>
-          </select>
+            <option value="合作聯繫">合作聯繫</option>
+            <option value="實際評測">實際評測</option>
+          </VField>
           <div v-if="form.type === '合作聯繫'">
             <div class="mb-2">
               <label for="cooperateCompany" class="form-label"
                 >公司<span class="text-danger">*</span></label
               >
-              <input
+              <VField
+                name="公司名稱"
                 type="text"
                 class="form-control"
                 id="cooperateCompany"
                 placeholder="公司名稱"
-                v-model="form.cooperateCompany"
+                v-model.trim="form.cooperateCompany"
+                rules="required"
+                :class="{ 'is-invalid': errors['公司名稱'] }"
               />
+              <ErrorMessage name="公司名稱" class="invalid-feedback" />
             </div>
             <div class="mb-3">
-              <label for="case" class="form-label">合作方案</label>
-              <textarea
+              <label for="case" class="form-label"
+                >合作方案<span class="text-danger">*</span></label
+              >
+              <VField
+                name="合作內容"
                 type="text"
                 class="form-control"
                 id="case"
                 placeholder="合作內容"
-                v-model="form.case"
+                v-model="form.cooperateContent"
                 rows="4"
-              ></textarea>
+                as="textarea"
+                rules="required"
+                :class="{ 'is-invalid': errors['合作內容'] }"
+              ></VField>
+              <ErrorMessage name="合作內容" class="invalid-feedback" />
             </div>
           </div>
           <div v-else-if="form.type === '實際評測'">
             <div class="row mb-2">
               <div class="col">
-                <label for="manufacturer" class="form-label">廠商</label>
-                <input
+                <label for="manufacturer" class="form-label"
+                  >廠商<span class="text-danger">*</span></label
+                >
+                <VField
+                  name="食品廠商"
                   id="manufacturer"
                   type="text"
                   class="form-control"
                   placeholder="食品廠商"
                   aria-label="First name"
-                  v-model="form.manufacturer"
+                  v-model.trim="form.manufacturer"
+                  rules="required"
+                  :class="{ 'is-invalid': errors['食品廠商'] }"
                 />
+                <ErrorMessage name="食品廠商" class="invalid-feedback" />
               </div>
               <div class="col">
-                <label for="recip" class="form-label">食材</label>
-                <input
+                <label for="recip" class="form-label">食材<span class="text-danger">*</span></label>
+                <VField
+                  name="食材"
                   id="recip"
                   type="text"
                   class="form-control"
                   placeholder="範例：珍珠/紅茶/貢丸"
-                  v-model="form.recipe"
+                  v-model.trim="form.recipe"
+                  rules="required"
+                  :class="{ 'is-invalid': errors['食材'] }"
                 />
+                <ErrorMessage name="食材" class="invalid-feedback" />
               </div>
             </div>
 
             <div class="mb-3">
-              <label class="form-label" for="experience">食譜與心得</label>
-              <textarea
+              <label class="form-label" for="experience"
+                >食譜與心得<span class="text-danger">*</span></label
+              >
+              <VField
+                name="食譜與心得"
                 class="form-control"
                 placeholder="食譜作法必填,包含心得,最少200字"
                 id="experience"
-                minlength="200"
                 rows="8"
                 v-model="form.experience"
-              ></textarea>
+                as="textarea"
+                rules="required|min:200"
+                :class="{ 'is-invalid': errors['食譜與心得'] }"
+              ></VField>
+              <ErrorMessage name="食譜與心得" class="invalid-feedback" />
             </div>
           </div>
-          <button type="submit" class="btn btn-primary w-100">寄出信件</button>
-        </form>
+          <button type="submit" class="btn btn-primary w-100" :disabled="isLoading">
+            <span
+              v-if="isLoading"
+              class="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+            寄出信件
+          </button>
+        </VForm>
       </div>
     </div>
   </div>
@@ -139,6 +192,8 @@
 import { ref } from 'vue'
 import { user } from '../../main'
 console.log(user)
+const isLoading = ref(false)
+const sendForm = ref(null)
 const form = ref({
   name: '',
   company: '',
@@ -146,12 +201,21 @@ const form = ref({
   phone: '',
   type: '',
   cooperateCompany: '',
-  case: '',
+  cooperateContent: '',
   manufacturer: '',
   recipe: '',
   experience: ''
 })
-const handleSubmit = async () => {
+const cleanType = () => {
+  form.value.manufacturer = ''
+  form.value.recipe = ''
+  form.value.experience = ''
+  form.value.cooperateCompany = ''
+  form.value.cooperateContent = ''
+}
+
+const onSubmit = async () => {
+  isLoading.value = true
   try {
     const response = await fetch('https://formspree.io/f/mzzbbjkj', {
       method: 'POST',
@@ -165,21 +229,23 @@ const handleSubmit = async () => {
         聯絡電話: form.value.phone,
         回饋方案: form.value.type,
         公司名稱: form.value.cooperateCompany,
-        合作方案: form.value.case,
+        合作方案: form.value.cooperateContent,
         食品廠商: form.value.manufacturer,
         食材: form.value.recipe,
         食譜與心得: form.value.experience
       })
     })
     if (response.ok) {
-      alert('Email sent successfully!')
-      form.value = {}
+      isLoading.value = false
+      alert('信件已寄出!')
+      sendForm.value.resetForm()
     } else {
-      alert('Failed to send email.')
+      alert('信件寄送失敗.')
     }
   } catch (error) {
+    isLoading.value = false
     console.error('Error:', error)
-    alert('Error occurred while sending email.')
+    alert('信件寄送失敗')
   }
 }
 </script>
