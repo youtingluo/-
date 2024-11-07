e8c2f33e5e4d90209280dccdb643717baba9c9de
youting <boy80314@gmail.com>
2024-11-07 16:18:36 +0800

    更新 deploy 內容

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
-
 # if you are deploying to a custom domain
 echo 'supply0.com' > CNAME
 
