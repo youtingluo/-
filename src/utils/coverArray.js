aa2c835b139e7e3c6e507cc17b0ba2a3bdaecca2
youting <boy80314@gmail.com>
2024-12-02 11:29:54 +0800

    函示單獨處出

--- src/utils/coverArray.js
+++ src/utils/coverArray.js
@@ -11,8 +11,37 @@ const convertToObjects = (array) => {
   const filterResult = result.map((item) => {
     return Object.fromEntries(Object.entries(item).filter(([, value]) => value))
   })
-
   return filterResult
 }
 
-export { convertToObjects }
+function covertAllObjects(array) {
+  const categorizedData = array.reduce((acc, item) => {
+    const filteredItem = Object.fromEntries(Object.entries(item).filter(([, value]) => value))
+    if (!acc[item.類別]) {
+      acc[item.類別] = []
+    }
+    acc[item.類別].push(filteredItem)
+    return acc
+  }, {})
+  const data = []
+  for (const key in categorizedData) {
+    data.push({ [key]: categorizedData[key] })
+  }
+  return data
+}
+
+const extractMatchingKeys = (dataArray, searchArray) => {
+  const extractedKeys = []
+  dataArray.forEach((dataItem) => {
+    Object.keys(dataItem).forEach((key) => {
+      searchArray.forEach((searchItem) => {
+        if (dataItem[key] && dataItem[key].includes(searchItem)) {
+          extractedKeys.push(key)
+        }
+      })
+    })
+  })
+  return [...new Set(extractedKeys)]
+}
+
+export { convertToObjects, covertAllObjects, extractMatchingKeys }
